f64d27978fdff3b5b56261fe8666c3629ac39827
Theerthana12 <theerthanapreddy@gmail.com>
2025-09-03 16:29:50 +0530

    api key environment changes

--- src/Searchbox.jsx
+++ src/Searchbox.jsx
@@ -7,7 +7,8 @@ export default function Searchbox({updateInfo}) {
   let [city,setCity]=useState("");
   let [error,setError]=useState(false);
   const API_URL="https://api.openweathermap.org/data/2.5/weather"
-  const API_KEY="85f692814f17086848a9828867960e55"
+  // const API_KEY="85f692814f17086848a9828867960e55"
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   let getWeatherInfo = async()=>{
        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
